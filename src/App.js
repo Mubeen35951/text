b1886c20d961983e3cff87a027bf21b5e9e25dc3
Mubeen35951 <166198879+Mubeen35951@users.noreply.github.com>
2024-06-28 12:04:36 +0500

    add env

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from './components/Home'
 import Router from './components/Router'
 import './App.css'
 const App = () => {
+  console.log(process.env.REACT_APP_API_KEY);
+  console.log(process.env.REACT_APP_OTHER_VALUE);
+
   return (
 <div>
 
